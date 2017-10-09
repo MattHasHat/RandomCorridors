2801b0f7745193010c2c3e68ad6170727aeb8498
MattHasHat <matthayworth@outlook.com>
2017-10-09 17:58:53 -0400

    Initial Commit

---
+++ Assets/Scripts/Dungeon.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dungeon : MonoBehaviour {
+
+    private enum TileSet
+    {
+        None,
+        Empty,
+        North, East, South, West,
+        NorthEast, NorthSouth, NorthWest,
+        EastSouth, EastWest, SouthWest,
+        NoNorth, NoEast, NoSouth, NoWest,
+        AllDirections,
+    }
+
+}
---
+++ Assets/Scripts/GridLocation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridLocation {
+
+    public int x;
+    public int y;
+
+    public GridLocation(int xLoc, int yLoc)
+    {
+        x = xLoc;
+        y = yLoc;
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (obj is GridLocation)
+        {
+            GridLocation t = (GridLocation)obj;
+            if (t.x == x && t.y == y)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+}
